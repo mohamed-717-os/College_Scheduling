8c9412b6bdd164968629fdca097eb5de558ff7b8
mohamed-717-os <mohamedsaead717@gmail.com>
2025-04-30 15:00:19 +0300

    connecting teachers with classes

--- scheduelModel.py
+++ scheduelModel.py
@@ -10,7 +10,8 @@ import json
     # /////////////////////  Data //////////////////////
 
 def scheduelModel():
-    with open('inputs/scheduling_inputs.json') as f:
+
+    with open('scheduling_inputs01.json') as f:
         data = json.load(f)
 
         HALLS = data['halls']  # number of halls
@@ -246,92 +247,122 @@ def scheduelModel():
                             f"GroupClass_{e}_{g}_{s}_{d}_{p}"
                         )
 
+                        for t in T:
+                            model += (
+                                pulp.lpSum(I[t,e,g,c,s,d,p] for c in classes[g]) == (len(classes[g]) * I[t,e,g,classes[g][0],s,d,p]),
+                                f"GroupDoctor_{t}_{e}_{g}_{s}_{d}_{p}"
+                            )
+
     # ---------------------------------------
 
-    # # Assistant subject Load 
-    # for a in A:
-    #     model += (
-    #         pulp.lpSum(ADS[a,s] for s in subj_list) <= AL[1],
-    #         f"AssistantLoadSubject_{a}"
-    #     )
+    # Assistant subject Load 
+    for a in A:
+        model += (
+            pulp.lpSum(ADS[a,s] for s in subj_list) <= AL[1],
+            f"AssistantLoadSubject_{a}"
+        )
 
-    # # Doctor subject Load 
-    # for t in T:
-    #     model += (
-    #         pulp.lpSum(TDS[t,s] for s in subj_list) <= TL[1],
-    #         f"DoctorLoadSubject_{t}"
-    #     )
+    # Doctor subject Load 
+    for t in T:
+        model += (
+            pulp.lpSum(TDS[t,s] for s in subj_list) <= TL[1],
+            f"DoctorLoadSubject_{t}"
+        )
 
     # Assistant period Load per week
-    # for a in A:
-    #     model += (
-    #         pulp.lpSum(J[a,e,g,c,s,d,p] for e in environments for g in groups[e] for c in classes[g] for s in subjects[e] for d in DAYS for p in PERIODS) <= AL[0],
-    #         f"AssistantLoad_{a}"
-    #     )
+    for a in A:
+        model += (
+            pulp.lpSum(J[a,e,g,c,s,d,p] for e in environments for g in groups[e] for c in classes[g] for s in subjects[e] for d in DAYS for p in PERIODS) <= AL[0],
+            f"AssistantLoad_{a}"
+        )
 
-    # # Doctor period Load per week
-    # for t in T:
-    #     model += (
-    #         pulp.lpSum(I[t,e,g,c,s,d,p] for e in environments for g in groups[e] for c in classes[g] for s in subjects[e] for d in DAYS for p in PERIODS) <= TL[0],
-    #         f"DoctorLoad_{t}"
-    #     )
+    # Doctor period Load per week
+    for t in T:
+        model += (
+            pulp.lpSum(I[t,e,g,classes[g][0],s,d,p] for e in environments for g in groups[e] for s in subjects[e] for d in DAYS for p in PERIODS) <= TL[0],
+            f"DoctorLoad_{t}"
+        )
 
     # ---------------------------------------
 
-    # # Each lecture must be assigned to exactly one Doctor
-    # for e in environments:
-    #     for g in groups[e]:
-    #         # Lecture is per group, assign based on the first class representation
-    #         c0 = classes[g][0]
-    #         for s in subjects[e]:
-    #             for d in DAYS:
-    #                 for p in PERIODS:
-    #                      # If a lecture is scheduled...
-    #                      model += (pulp.lpSum(I[t, e, g, c0, s, d, p] for t in T) == Y[e, g, c0, s, d, p],
-    #                                f"AssignDoctorToLecture_{e}_{g}_{s}_{d}_{p}")
+    # Linking the doctors and assistants to the lectures and sections
+    for e in environments:
+        for g in groups[e]:
+            for c in classes[g]:
+                for s in subjects[e]:
+                    for d in DAYS:
+                        for p in PERIODS:
+                            model += (pulp.lpSum(I[t, e, g, c, s, d, p] for t in T) == Y[e, g, c, s, d, p],
+                                    f"AssignDoctorToLecture_{e}_{g}_{c}_{s}_{d}_{p}")
 
-    # Each section must be assigned to exactly one Assistant
-    # for e in environments:
-    #     for g in groups[e]:
-    #         for c in classes[g]:
-    #             for s in subjects[e]:
-    #                 for d in DAYS:
-    #                     for p in PERIODS:
-    #                         # If a section is scheduled...
-    #                         model += (pulp.lpSum(J[a, e, g, c, s, d, p] for a in A) == X[e, g, c, s, d, p],
-    #                                   f"AssignTAToSection_{e}_{g}_{c}_{s}_{d}_{p}")
+    for e in environments:
+        for g in groups[e]:
+            for c in classes[g]:
+                for s in subjects[e]:
+                    for d in DAYS:
+                        for p in PERIODS:
+                            model += (pulp.lpSum(J[a, e, g, c, s, d, p] for a in A) == X[e, g, c, s, d, p],
+                                    f"AssignTAToSection_{e}_{g}_{c}_{s}_{d}_{p}")
 
-    # for a in A:
-    #     for s in subj_list:
-    #         model += (
-    #             pulp.lpSum(J[a, e, g, c, s, d, p]
-    #                        for e in environments
-    #                        for g in groups[e]
-    #                        for c in classes[g]
-    #                        for d in DAYS
-    #                        for p in PERIODS)
-    #             <= ADS[a, s] * AL[0],
-    #             f"LinkADS_ub_{a}_{s}"
-    #         )
-    #         model += (
-    #             pulp.lpSum(J[a, e, g, c, s, d, p]
-    #                        for e in environments
-    #                        for g in groups[e]
-    #                        for c in classes[g]
-    #                        for d in DAYS
-    #                        for p in PERIODS)
-    #             >= ADS[a, s],
-    #             f"LinkADS_lb_{a}_{s}"
-    #         )  
+    for s in subj_list:
+        for a in A:
+            model += (
+                pulp.lpSum(J[a, e, g, c, s, d, p]
+                        for e in environments
+                        for g in groups[e]
+                        for c in classes[g]
+                        for d in DAYS
+                        for p in PERIODS)
+                <= ADS[a, s] * AL[0],
+                f"LinkADS_ub_{a}_{s}"
+            )
+            model += (
+                pulp.lpSum(J[a, e, g, c, s, d, p]
+                        for e in environments
+                        for g in groups[e]
+                        for c in classes[g]
+                        for d in DAYS
+                        for p in PERIODS)
+                >= ADS[a, s],
+                f"LinkADS_lb_{a}_{s}"
+            )  
 
-    # for a in A:
-    #     for d in DAYS:
-    #         for p in PERIODS:
-    #             model += (
-    #                 pulp.lpSum(J[a, e, g, c, s, d, p] for e in environments for g in groups[e] for c in classes[g] for s in subjects[e]) <= 1,
-    #                 f"AssignTAToSection_{a}_{d}_{p}"
-    #             )
+        for t in T:
+            model += (
+                pulp.lpSum(I[t, e, g, classes[g][0], s, d, p]
+                        for e in environments
+                        for g in groups[e]
+                        for d in DAYS
+                        for p in PERIODS)
+                <= TDS[t, s] * TL[0],
+                f"LinkTDS_ub_{t}_{s}"
+            )
+            model += (
+                pulp.lpSum(I[t, e, g, classes[g][0], s, d, p]
+                        for e in environments
+                        for g in groups[e]
+                        for d in DAYS
+                        for p in PERIODS)
+                >= TDS[t, s],
+                f"LinkTDS_lb_{t}_{s}"
+            )  
 
+    # assistants and doctors have just 1 subject in the single period
+    for a in A:
+        for d in DAYS:
+            for p in PERIODS:
+                model += (
+                    pulp.lpSum(J[a, e, g, c, s, d, p] for e in environments for g in groups[e] for c in classes[g] for s in subjects[e]) <= 1,
+                    f"AssignTAToSection_{a}_{d}_{p}"
+                )
+
+    for t in T:
+        for d in DAYS:
+            for p in PERIODS:
+                model += (
+                    pulp.lpSum(I[t, e, g, classes[g][0], s, d, p] for e in environments for g in groups[e] for s in subjects[e]) <= 1,
+                    f"AssignTAToLecture_{t}_{d}_{p}"
+                )
     # ---------------------------------------
     for e in environments:
         for g in groups[e]:
@@ -414,18 +445,18 @@ def scheduelModel():
     
     # === Objective ===
     model += (
-        pulp.lpSum(3 * DEV[e,g,c,d] +  70 * BD[e,g,c,d] +  10 * GAP[e,g,c,d] for e in environments for g in groups[e] for c in classes[g] for d in DAYS)
-        # - pulp.lpSum( pulp.lpSum( J[a,e,g,c,s,d,p] * AT[a][str(d)][str(p)] for e in environments for g in groups[e] for c in classes[g] for s in subjects[e]) for a in A for d in DAYS for p in PERIODS)
-        # - pulp.lpSum( pulp.lpSum( I[t,e,g,c,s,d,p] * TT[t][str(d)][str(p)] for e in environments for g in groups[e] for c in classes[g] for s in subjects[e]) for t in T for d in DAYS for p in PERIODS)
-        # - pulp.lpSum(ADS[a,s] * AS[a][s] for a in A for s in subj_list)
-        # - pulp.lpSum(TDS[t,s] * TS[t][s] for t in T for s in subj_list)
+        pulp.lpSum( 9* DEV[e,g,c,d] +   25 * BD[e,g,c,d] +   30 * GAP[e,g,c,d] for e in environments for g in groups[e] for c in classes[g] for d in DAYS)
+        - pulp.lpSum( pulp.lpSum( J[a,e,g,c,s,d,p] * AT[a][str(d)][str(p)] for e in environments for g in groups[e] for c in classes[g] for s in subjects[e]) for a in A for d in DAYS for p in PERIODS)
+        - pulp.lpSum( pulp.lpSum( I[t,e,g,c,s,d,p] * TT[t][str(d)][str(p)] for e in environments for g in groups[e] for c in classes[g] for s in subjects[e]) for t in T for d in DAYS for p in PERIODS)
+        - pulp.lpSum(ADS[a,s] * AS[a][s] for a in A for s in subj_list)
+        - pulp.lpSum(TDS[t,s] * TS[t][s] for t in T for s in subj_list)
         , "MinimizeStudyDays"
     )
 
 
     # === Solve ===
     # model.solve(pulp.PULP_CBC_CMD(msg=1))
-    model.solve(pulp.PULP_CBC_CMD(msg=True, timeLimit=3))  # Limit to 30 seconds
+    model.solve(pulp.PULP_CBC_CMD(msg=True, timeLimit=120))  # Limit to 30 seconds
 
     # === Results ===
     print("Status:", pulp.LpStatus[model.status])
@@ -455,15 +486,17 @@ def scheduelModel():
 
                     if val_lecture and val_lecture > 0.5:
                         lecture_found = True
-                        # for a in A:
-                        # if pulp.value(J[a,environment, group, class_name, s, d, p]):
-                        subjects_str += f"Lec: {s}"
+                        for t in T:
+                            if pulp.value(I[t,environment, group, class_name, s, d, p]):
+                                # print(f'{group}{class_name} {d} {p} Lec:{s}: {t}')
+                                subjects_str += f"Lec:{s}: {t}"
 
                     if val_section and val_section > 0.5:
                         section_found = True
-                        # for t in T:
-                        # if pulp.value(I[t,environment, group, class_name, s, d, p]):
-                        subjects_str += f"Sec: {s}"
+                        for a in A:
+                            if pulp.value(J[a,environment, group, class_name, s, d, p]):
+                                subjects_str += f"Sec:{s}: {a}"
+
 
                 row_text.append(subjects_str.strip())
 
@@ -495,6 +528,115 @@ def scheduelModel():
         plt.savefig(img_path)
         plt.close(fig)  
 
+    def Teacher_schedule(a, subj_list, path):
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.set_axis_off()
+
+        # Table setup
+        cell_text = []
+        cell_colors = []
+
+        for d in DAYS:
+            row_text = []
+            row_colors = []
+
+            for p in PERIODS:
+                subjects_str = ""
+                section_found = False
+                
+                for s in subj_list:
+                    for e in environments:
+                        for g in groups[e]: 
+                            for c in classes[g]:
+                                val_assistant = pulp.value(J[a, e, g, c, s, d, p])
+
+                                if val_assistant and val_assistant > 0.5:
+                                    subjects_str += f"sec:{c} - {s}"
+                                    section_found = True
+
+
+                row_text.append(subjects_str.strip())
+
+                if section_found:
+                    row_colors.append("lightgreen")
+                else:
+                    row_colors.append("whitesmoke")
+
+            cell_text.append(row_text)
+            cell_colors.append(row_colors)
+
+        # Create the table
+        table = ax.table(cellText=cell_text,
+                        cellColours=cell_colors,
+                        colLabels=[f"P{p}" for p in PERIODS],
+                        rowLabels=[f"Day {d}" for d in DAYS],
+                        cellLoc='center',
+                        loc='center',)
+
+        table.auto_set_font_size(False)
+        table.set_fontsize(10)
+        table.scale(1.2, 1.2)
+
+        plt.title(f"Schedule for {a}", fontsize=14)
+        plt.tight_layout()
+        img_path = os.path.join(path, f"{a}.png")
+        plt.savefig(img_path)
+        plt.close(fig)  
+
+    def Doctor_schedule(t, subj_list, path):
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.set_axis_off()
+
+        # Table setup
+        cell_text = []
+        cell_colors = []
+
+        for d in DAYS:
+            row_text = []
+            row_colors = []
+
+            for p in PERIODS:
+                subjects_str = ""
+                lecture_found = False
+                
+                for s in subj_list:
+                    for e in environments:
+                        for g in groups[e]: 
+                            val_doctor = pulp.value(I[t, e, g, classes[g][0], s, d, p])
+
+                            if val_doctor and val_doctor > 0.5:
+                                subjects_str += f"group:{g} - {s}"
+                                lecture_found = True
+
+
+                row_text.append(subjects_str.strip())
+
+                if lecture_found:
+                    row_colors.append("lightblue")
+                else:
+                    row_colors.append("whitesmoke")
+
+            cell_text.append(row_text)
+            cell_colors.append(row_colors)
+
+        # Create the table
+        table = ax.table(cellText=cell_text,
+                        cellColours=cell_colors,
+                        colLabels=[f"P{p}" for p in PERIODS],
+                        rowLabels=[f"Day {d}" for d in DAYS],
+                        cellLoc='center',
+                        loc='center')
+
+        table.auto_set_font_size(False)
+        table.set_fontsize(10)
+        table.scale(1.2, 1.2)
+
+        plt.title(f"Schedule for {t}", fontsize=14)
+        plt.tight_layout()
+        img_path = os.path.join(path, f"{t}.png")
+        plt.savefig(img_path)
+        plt.close(fig)  
+
     # print schedule for year1 groups
     os.makedirs("schedule", exist_ok=True)
 
@@ -514,41 +656,13 @@ def scheduelModel():
                 for d in DAYS:
                     gaps +=  pulp.value(GAP[e,g,c,d])
                     days += pulp.value(BD[e,g,c,d])
-                    print(f'{g}{c} {d} gap = {pulp.value(GAP[e,g,c,d])} = {pulp.value(LP[e,g,c,d])} - {pulp.value(FP[e,g,c,d])} + 1 - {pulp.value(Load[e,g,c,d])}')
-                    print(f'{g}{c} {d} DEV = {pulp.value(DEV[e,g,c,d])} > {pulp.value(Load[e,g,c,d])} - {math.ceil(len(PERIODS)/2)} - {math.ceil(len(PERIODS)/2)}*(1 - {pulp.value(BD[e,g,c,d])})')
-                    print(f'{g}{c} {d} BD = {pulp.value(BD[e,g,c,d])}\n')
-                print()
-            print()
-        print('--------------------------------')
 
+    assist_dir = os.path.join("schedule", 'assistants')
+    for a in A:
+        os.makedirs(assist_dir, exist_ok=True)  # Create subfolder for environment
+        Teacher_schedule(a, subj_list, assist_dir)
 
-
-    print(f'\nall gaps: {gaps}')
-    print(f'\nall days: {days}')
-    print('\nLecture\n')
-
-    print('   ', ''.join(f'{p:^5} ' for p in PERIODS))
-
-    for d in DAYS:
-        print('d1: ', end='')
-        for p in PERIODS:
-            periodSub = 0
-            for e in environments:
-                for g in groups[e]:
-                    for s in subjects[e]:
-                        periodSub += pulp.value(Y[e,g,classes[g][0],s,d,p])
-            print(f'{periodSub:^5} ', end='')
-        print()
-
-    print('\nSection\n')
-    for d in DAYS:
-        print('d1: ', end='')
-        for p in PERIODS:
-            periodSub = 0
-            for e in environments:
-                for g in groups[e]:
-                    for c in classes[g]:
-                        for s in subjects[e]:
-                            periodSub += pulp.value(X[e,g,c,s,d,p])
-            print(f'{periodSub:^5} ', end='')
-        print()
+    doc_dir = os.path.join("schedule", 'doctors')
+    for t in T:
+        os.makedirs(doc_dir, exist_ok=True)  # Create subfolder for environment
+        Doctor_schedule(t, subj_list, doc_dir)
